951d8abbed6759971005bd10fe8453afbbdcf591
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-24 19:24:14 +0530

    [React Frontend]: Page Heading Component created and implemented.

--- eazystore-ui/src/components/Home.jsx
+++ eazystore-ui/src/components/Home.jsx
@@ -1,4 +1,12 @@
+import PageHeading from "./PageHeading";
 
 export default function Home()  {
-  return <div className="home-container"></div>
+  return (
+    <div className="home-container">
+      <PageHeading title="Explore Eazy Stickers!">
+        Add a touch of creativity to your space with your wide range of fun and
+        unique stickers. Perfect for any occasion!
+      </PageHeading>
+    </div>
+  );
 };
---
+++ eazystore-ui/src/components/PageTitle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function PageTitle(props) {
+  return (
+    <h1 className="page-title">{props.title}</h1>
+  );
+}
