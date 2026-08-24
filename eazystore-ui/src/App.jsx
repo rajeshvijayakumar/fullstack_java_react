8975e60b999afe72e26cedda6b6bba4238351806
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-24 12:43:19 +0530

    [ReactFrontend]: Footer UI Component created.

--- eazystore-ui/src/App.jsx
+++ eazystore-ui/src/App.jsx
@@ -1,12 +1,16 @@
-import ClassHeader from "./components/ClassHeader";
-import Header from "./components/Header"
-
-Header
-
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./components/Home";
 
 function App() {
-
-  return <ClassHeader />;
+  return (
+    <React.Fragment>
+      <Header />
+      <Home />
+      <Footer />
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
