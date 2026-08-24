40c127af0390768fefd3b5560f6cb5e7cdbf30e8
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-24 12:00:20 +0530

    Removed Header class component react.

--- eazystore-ui/src/components/ClassHeader.jsx
+++ eazystore-ui/src/components/ClassHeader.jsx
@@ -1,54 +1,54 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingBasket, faTags } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faShoppingBasket, faTags } from "@fortawesome/free-solid-svg-icons";
 
-import { Component } from "react";
+// import { Component } from "react";
 
-class ClassHeader extends Component {
+// class ClassHeader extends Component {
 
-  render() {
-    return (
-      <header className="header">
-        <div className="container">
-          <a href="/" className="link">
-            <FontAwesomeIcon icon={faTags} className="fa-icon" />
-            <span className="brand-title">Eazy Stickers</span>
-          </a>
-          <nav className="nav">
-            <ul>
-              <li>
-                <a href="/" className="nav-link">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="/" className="nav-link">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="/" className="nav-link">
-                  Contact
-                </a>
-              </li>
-              <li>
-                <a href="/" className="nav-link">
-                  Login
-                </a>
-              </li>
-              <li>
-                <a href="/" className="nav-link">
-                  <FontAwesomeIcon
-                    icon={faShoppingBasket}
-                    className="fa-icon"
-                  />
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <header className="header">
+//         <div className="container">
+//           <a href="/" className="link">
+//             <FontAwesomeIcon icon={faTags} className="fa-icon" />
+//             <span className="brand-title">Eazy Stickers</span>
+//           </a>
+//           <nav className="nav">
+//             <ul>
+//               <li>
+//                 <a href="/" className="nav-link">
+//                   Home
+//                 </a>
+//               </li>
+//               <li>
+//                 <a href="/" className="nav-link">
+//                   About
+//                 </a>
+//               </li>
+//               <li>
+//                 <a href="/" className="nav-link">
+//                   Contact
+//                 </a>
+//               </li>
+//               <li>
+//                 <a href="/" className="nav-link">
+//                   Login
+//                 </a>
+//               </li>
+//               <li>
+//                 <a href="/" className="nav-link">
+//                   <FontAwesomeIcon
+//                     icon={faShoppingBasket}
+//                     className="fa-icon"
+//                   />
+//                 </a>
+//               </li>
+//             </ul>
+//           </nav>
+//         </div>
+//       </header>
+//     );
+//   }
+// }
 
-export default ClassHeader;
+// export default ClassHeader;
