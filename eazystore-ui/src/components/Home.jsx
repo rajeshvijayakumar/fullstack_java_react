355c96870983c1704be28be8b810d721068167b1
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-25 14:50:44 +0530

    [ReactFrontend]: ProductListing and Details component created with dummy empty data set.

--- eazystore-ui/src/components/Home.jsx
+++ eazystore-ui/src/components/Home.jsx
@@ -1,12 +1,15 @@
 import PageHeading from "./PageHeading";
+import ProductListings from "./ProductListings";
 
 export default function Home()  {
+  const products = [];
   return (
     <div className="home-container">
       <PageHeading title="Explore Eazy Stickers!">
         Add a touch of creativity to your space with your wide range of fun and
         unique stickers. Perfect for any occasion!
       </PageHeading>
+      <ProductListings products={products}/>
     </div>
   );
 };
