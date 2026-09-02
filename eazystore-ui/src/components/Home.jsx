0fdf84cabd367f6fd92f82eec6d87769f70717ef
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-02 11:56:54 +0530

    [Update]: Products api from spring boot integrated with front-end react app..

--- eazystore-ui/src/components/Home.jsx
+++ eazystore-ui/src/components/Home.jsx
@@ -1,8 +1,22 @@
 import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
-import products from "../data/product";
+import apiClient from "../api/apiClient";
+import { useState, useEffect } from "react";
 
-export default function Home()  {
+export default function Home() {
+  const [products, setProducts] = useState([]);
+  
+  // Run once when the component mounts
+  // Mounting is the process of creating and adding the component into DOM
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const response = await apiClient.get("/products");
+    setProducts(response.data);
+  };
+
   return (
     <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Explore Eazy Stickers!">
@@ -12,4 +26,4 @@ export default function Home()  {
       <ProductListings products={products} />
     </div>
   );
-};
+}
--- eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -1,17 +1,27 @@
 package com.eazybytes.eazystore.controller;
 
 
+import com.eazybytes.eazystore.dto.ProductDto;
+import com.eazybytes.eazystore.service.IProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
 
-    @GetMapping
-    public String getProducts() {
+    private final IProductService iProductService;
 
-        return "Here are your products";
+    @GetMapping
+    public List<ProductDto> getProducts() {
+
+        List<ProductDto> productList = iProductService.getProducts();
+
+        return productList;
     }
 }
